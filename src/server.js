8dd7e9127bde40ff0942a0171e4efb8fbd4e6e97
Jakub <jakubkarwowski95@gmail.com>
2022-10-10 18:25:45 +0200

    configuring cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require("dotenv").config();
 const connectDB = require("./Config/db");
-var bodyParser = require("body-parser");
-var cors = require('cors');
+const bodyParser = require("body-parser");
+const cors = require('cors');
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    allowedHeaders: "*",
+    allowedMethods: "*",
+    origin: "*",
+}));
 const port = process.env.PORT || 5000;
 
 connectDB();
